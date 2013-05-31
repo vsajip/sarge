268d02201d8c536eb81d319fee5d018aeb3d4fda
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2013-05-31 23:26:05 +0100

    Improved slow_lister options.

--- slow_lister.py
+++ slow_lister.py
@@ -1,34 +1,64 @@
 import logging
+import optparse
 import os
+import re
 import sys
 import time
 
 logger = logging.getLogger(__name__)
 
-def main(args=None):
-    if args is None:
-        args = sys.argv[1:]
-    if not args:
-        sys.stderr.write('usage: slow_lister <filename>\n')
-        return 1
-    fn = args[0]
-    if not os.path.isfile(fn):
-        sys.stderr.write('not a file: %r\n' % fn)
-        return 2
-    bytes_written = 0
+def _file_lines(fn):
     with open(fn) as f:
         for line in f:
-            sys.stdout.write(line)
-            sys.stdout.flush()
-            bytes_written += len(line)
-            if '<head>' in line or '<body>' in line:
-                s = ': %r' % line
-                level = logging.INFO
-            else:
-                s = ''
-                level = logging.DEBUG
-            logger.log(level, 'Wrote out %d bytes%s', bytes_written, s)
-            time.sleep(0.25)
+            yield line
+
+def _auto_lines():
+    i = 1
+    while True:
+        line = 'line %d\n' % i
+        i += 1
+        yield line
+
+def main(args=None):
+    parser = optparse.OptionParser(usage='usage: %prog [options] [filename]',
+                                   description='Print lines optionally from '
+                                               'a file, with a delay between '
+                                               'lines. If no filename is '
+                                               'specified, lines of the form '
+                                               '"line N" are generated '
+                                               'internally and printed.')
+    parser.add_option('-d', '--delay', default=None, type=float,
+                      help='Delay between lines (seconds)')
+    parser.add_option('-i', '--interest', default=None,
+                      help='Indicate patterns of interest for logging')
+    if args is None:
+        args = sys.argv[1:]
+    options, args = parser.parse_args(args)
+    if not args:
+        liner = _auto_lines()
+    else:
+        fn = args[0]
+        if not os.path.isfile(fn):
+            sys.stderr.write('not a file: %r\n' % fn)
+            return 2
+        liner = _file_lines(fn)
+    bytes_written = 0
+    pattern = options.interest
+    if pattern:
+        pattern = re.compile(pattern)
+    for line in liner:
+        sys.stdout.write(line)
+        sys.stdout.flush()
+        bytes_written += len(line)
+        if pattern and pattern.search(line):
+            s = ': %r' % line
+            level = logging.INFO
+        else:
+            s = ''
+            level = logging.DEBUG
+        logger.log(level, 'Wrote out %d bytes%s', bytes_written, s)
+        if options.delay:
+            time.sleep(options.delay)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, filename='slow_lister.log',
