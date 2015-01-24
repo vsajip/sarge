cd8a5cc94a35023be51b174ddd7fa376c899103e
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2015-01-24 15:04:40 +0000

    Updated classifiers.

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
