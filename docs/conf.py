d34426c9cfbf7017eac3cd2f6c250104ccda5ca2
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2019-04-09 17:19:19 +0100

    Standardised theme environment variable.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = os.environ.get('SARGE_DOCS_THEME', 'default')
+html_theme = os.environ.get('DOCS_THEME', 'default')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
