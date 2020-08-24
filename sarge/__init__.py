b94be5e156eff64c7173a33b0217dec941eaf8ef
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2020-08-24 14:00:34 +0100

    Bump version.

--- sarge/__init__.py
+++ sarge/__init__.py
@@ -37,7 +37,7 @@ __all__ = ('shell_quote', 'Capture', 'Command', 'ShellFormatter', 'Pipeline',
            'shell_format', 'run', 'parse_command_line',
            'capture_stdout', 'capture_stderr', 'capture_both')
 
-__version__ = '0.1.6'
+__version__ = '0.1.7.dev0'
 
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
